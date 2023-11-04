02b264c80b075388ea5afbc16c71476ddc59ea47
王涛 <wangtao@wangtaodeAir.mshome.net>
2023-11-04 23:38:26 +0800

    测试打包

---
+++ assets/FrameBlank.420d2017.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.f4b7fc3b.js";import{A as n,B as r,D as a}from"./vendor.97df4062.js";const o=n({name:"FrameBlank"});function t(s,c,f,m,p,d){return r(),a("div")}var l=e(o,[["render",t]]);export{l as default};
